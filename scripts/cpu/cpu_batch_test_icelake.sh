d249eb3e74f283fab70149112816e999ce550c31
zachary_amiton@brown.edu <zamiton@node1901.oscar.ccv.brown.edu>
2024-05-03 01:00:00 -0400

    wrote scripts for the cpus we want to use + flags we could feasibly test

---
+++ scripts/cpu/cpu_batch_test_icelake.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Attempt to request a specific 
+
+#SBATCH -p batch
+#SBATCH -C icelake
+
+# Ensures all allocated cores are on the same node
+#SBATCH -N 1
+
+# Request 1 CPU core
+#SBATCH -n 1
+
+#SBATCH -t 00:15:00
+#SBATCH -o outputs/with_cpu_icelake-%j.out
+#SBATCH -e outputs/with_cpu_icelake-%j.err
+
+# Load CUDA module
+echo "~~~ NATIVE ~~~";
+julia -Cnative benchmark.jl CPU;
+
+for cf in branchfusion cmov harden-sls-ijmp harden-sls-ret lvi-load-hardening macrofusion pad-short-functions sse; 
+do 
+	julia -Cnative,+$cf benchmark.jl CPU; 
+done;
